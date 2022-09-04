d78f8f05948f46a25982635302556f2f67e29ba4
artyuhvladislav <workto22@gmail.com>
2022-09-05 02:17:57 +0300

    add gh-pages

--- README.md
+++
@@ -1,2 +0,0 @@
-# cut2code
-layout
--- src/js/dom.js
+++
@@ -1,9 +0,0 @@
-export const $ = {};
-
-$.mobileMenu = document.getElementById('mobile-menu');
-$.mobileMenuNav = document.querySelector('.header__mobile-menu');
-$.header = document.querySelector('.header');
-$.tabs = document.querySelector('.tabs');
-$.sliderList = document.querySelector('#slider-list');
-$.bullets = document.querySelector('.glide__bullets');
-$.subscribe = document.querySelector('#subscribe-button');
